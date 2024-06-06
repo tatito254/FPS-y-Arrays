8d8c48d7d71a14e1d290dace3e4fd9ed31e6ed2c
tatito254 <164010118+tatito254@users.noreply.github.com>
2024-06-06 09:07:39 -0300

    Initial commit

---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public int NivelHambre;
+    public FoodScript Alimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TomarAlimento(int ValorAlimentario)
+    {
+        NivelHambre -= ValorAlimentario;
+        if (NivelHambre <= 0)
+        {
+            NivelHambre = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        FoodScript Alimento;
+        Alimento = other.GetComponent<FoodScript>();
+        TomarAlimento(Alimento.ValorAlimentario);
+    }
+
+}
